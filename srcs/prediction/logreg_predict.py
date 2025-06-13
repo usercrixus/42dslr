4857d097e482e73327f2268f0b2f4674f89325b0
Gaetan Morel <gmorel@f1r9s14.clusters.42paris.fr>
2025-06-13 17:41:48 +0200

    works

--- srcs/prediction/logreg_predict.py
+++ srcs/prediction/logreg_predict.py
@@ -2,13 +2,16 @@ import sys
 import numpy as np
 import pandas as pd
 
-def softmax(z):
-	exp_z = np.exp(z - np.max(z))
-	return exp_z / exp_z.sum()
-
-def main(dataset, data_test):
-	df_dataset = pd.read_csv(dataset)
-	df_dataset = df_dataset.drop(columns=['Id'], errors='ignore')
+def normalize(data_test):
+	df_ref = pd.read_csv("../../datasets/dataset_train.csv")
+	df_ref = df_ref.drop(columns=[
+		'Index',
+		'First Name',
+		'Last Name',
+		'Best Hand',
+		'Hogwarts House',
+		'Birthday'
+	], errors='ignore')
 	df_datatest = pd.read_csv(data_test)
 	df_datatest = df_datatest.drop(columns=[
 		'Index',
@@ -18,9 +21,20 @@ def main(dataset, data_test):
 		'Hogwarts House',
 		'Birthday'
 	], errors='ignore')
+	df_datatest = (df_datatest - df_ref.min()) / (df_ref.max() - df_ref.min())
 	df_datatest['Biais'] = 1
 	cols = ['Biais'] + [col for col in df_datatest.columns if col != 'Biais']
 	df_datatest = df_datatest[cols]
+	return df_datatest
+
+def softmax(z):
+	exp_z = np.exp(z - np.max(z))
+	return exp_z / exp_z.sum()
+
+def main(dataset, data_test):
+	df_dataset = pd.read_csv(dataset)
+	df_dataset = df_dataset.drop(columns=['Id'], errors='ignore')
+	df_datatest = normalize(data_test)
 	for j in range(len(df_datatest)):
 		z = [0, 0, 0, 0]
 		line = df_datatest.iloc[j]
@@ -31,9 +45,9 @@ def main(dataset, data_test):
 				z[k] += weight * feature
 		p = softmax(z)
 		predicted_index = np.argmax(p)
-		houses = ["Ravenclaw", "Gryffindor", "Slytherin", "Hufflepuff"]
+		houses = ["Ravenclaw", "Slytherin", "Gryffindor", "Hufflepuff"]
 		predicted_house = houses[predicted_index]
-		print(f"Ligne {j} prédite : {predicted_house}")
+		print(f"{j},{predicted_house}")
 	
 if __name__=='__main__':
     if len(sys.argv) != 3:
--- srcs/training/utils/train.py
+++ srcs/training/utils/train.py
@@ -20,7 +20,8 @@ def training(df_normalized):
 	df = pd.read_csv('dataset.csv')
 	df = df.astype(float)
 	df = df.drop(columns=['Id'], errors='ignore')
-	for epoch in range(30):
+	for epoch in range(5):
+		accuracy = 0
 		for j in range(len(df_normalized)):
 			z = [0, 0, 0, 0]
 			line = df_normalized.iloc[j]
@@ -48,6 +49,17 @@ def training(df_normalized):
 				for k in range(4):
 					error = p[k] - y[k]
 					df.iat[k, i] -= learning_rate * error * line.iloc[i]
+			id = p.argmax()
+			if id == 0 and good_result == "Ravenclaw":
+				accuracy += 1
+			if id == 1 and good_result == "Slytherin":
+				accuracy += 1
+			if id == 2 and good_result == "Gryffindor":
+				accuracy += 1
+			if id == 3 and good_result == "Hufflepuff":
+				accuracy += 1
+		accuracy = (accuracy / len(df_normalized)) * 100
+		print(f"Accuracy : {accuracy}")
 	df['Id'] = [1, 2, 3, 4]
 	cols = ['Id'] + [col for col in df.columns if col not in ['Id']]
 	df = df[cols]
